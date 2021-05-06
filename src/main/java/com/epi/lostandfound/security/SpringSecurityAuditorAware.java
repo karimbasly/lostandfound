f88645dce0f9d5676c439d621011edeb8eed17be
selimsakly <selimsakly@gmail.com>
2021-05-06 14:39:18 +0100

    Initial version of lostandfound generated by JHipster-7.0.1

---
+++ src/main/java/com/epi/lostandfound/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.epi.lostandfound.security;
+
+import com.epi.lostandfound.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
